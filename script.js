78caaf0c4edf5084a086f84b2ca0099281baedbf
darkosel3 <darkosel7@gmail.com>
2022-08-08 14:50:50 +0200

    Edit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ['Rock', 'Paper', 'Scissors'];
 
 const getComputerChoice = function () {
   let randomChoice = Math.floor( Math.random() * 3 );
@@ -10,15 +10,22 @@ const getComputerChoice = function () {
 
 const playRound = function ( playerSelection, computerSelection ) {
   let player = playerSelection.charAt( 0 ).toUpperCase() + playerSelection.slice( 1 ).toLowerCase();
-  let computer = computerSelection.charAt( 0 ).toUpperCase() + computerSelection.slice( 1 ).toLowerCase();
+  let computer = computerSelection;
 
   if ( player === computer ) {
-    console.log( "It's a draw" );
+    return `It's a draw ${player} vs ${computer} is equal!`;
   } else if ( ( computer === 'Rock' && player === 'Scissors' ) || ( computer === 'Paper' && player === 'Rock' ) ) {
     return `You Lose! ${computer} beats ${player}`;
   } else {
-    return `You Lose! ${player} beats ${computer}`;
+    return `You Win! ${player} beats ${computer}`;
   }
 };
+
+const game = function () {
+  while ()
+};
+
+
 const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
+
