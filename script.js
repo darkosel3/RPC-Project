2459c5d1c9aefd0000f037695940f9f7feffd679
darkosel3 <darkosel7@gmail.com>
2022-08-12 12:22:22 +0200

    Second UI branch

--- script.js
+++ script.js
@@ -13,19 +13,44 @@ const playRound = function ( playerSelection, computerSelection ) {
   let computer = computerSelection;
 
   if ( player === computer ) {
-    return `It's a draw ${player} vs ${computer} is equal!`;
+    return 2;
   } else if ( ( computer === 'Rock' && player === 'Scissors' ) || ( computer === 'Paper' && player === 'Rock' ) ) {
-    return `You Lose! ${computer} beats ${player}`;
+    return 0;
   } else {
-    return `You Win! ${player} beats ${computer}`;
+    return 1;
   }
 };
 
-const game = function () {
-  while ()
+const stringResult = function ( result, player, computer ) {
+  if ( result === 0 ) {
+    return `You Lose! ${computer} beats ${player}`;
+  } else if ( result === 1 ) {
+    return `You Win! ${player} beats ${computer}`;
+  } else {
+    return `${player} vs ${computer} is equal!`;
+  }
 };
 
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-
+// prompt( "Choose rock,paper or scissors" )
+const game = function () {
+  let score = [0, 0];
+  while ( score[0] <= 5 || score[1] <= 5 ) {
+    let playerSelection = prompt( 'Choose Rock,Paper,Scissors' );
+    console.log( playerSelection );
+    let computerSelection = getComputerChoice();
+    let result = playRound( playerSelection, computerSelection );
+    let string = stringResult( result, playerSelection, computerSelection );
+    if ( result < 2 ) {
+      score[result]++;
+    } else {
+      score[0]++;
+      score[1]++;
+    };
+    alert( string + `,Computer ${score[1]} Player ${score[0]}` );
+  }
+};
+// `You Lose! ${computer} beats ${player}` - 0
+// `You Win! ${player} beats ${computer}` - 1
+// `It's a draw ${player} vs ${computer} is equal!` - 2
+game();
